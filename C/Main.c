83deb647c97754a81efc373df1f8f718fbe04618
Daniel Moreno <danielmorenoperez071103@gmail.com>
2022-09-18 21:48:36 +0200

    actualización

---
+++ C/Main.c
@@ -0,0 +1,15 @@
+
+
+int main()
+{
+    int a = 0;
+    while (a != 0)
+    {
+        print("_________________");
+        print("");
+        return 0;
+
+    }
+    
+    
+}
--- Java/Main.java
+++ Java/Main.java
@@ -7,7 +7,8 @@ public class Main{
             System.out.println(" __________________________");
             System.out.println("|                          |");
             System.out.println(" --------------------------");}
-            System.out.println("Hola");
+            Runtime.getRuntime().exec("cls");
+
     }
 }
 
