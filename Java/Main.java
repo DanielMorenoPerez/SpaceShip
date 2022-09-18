6697f94c4bf8486a40da143271daa9f58b06a070
Daniel Moreno <danielmorenoperez071103@gmail.com>
2022-09-18 18:59:59 +0200

    Create Main.java

---
+++ Java/Main.java
@@ -0,0 +1,13 @@
+import java.io.PrintStream;
+
+public class Main{
+    public static void main(String[] args) {
+        int a = 0;
+        while(a != 0){
+            System.out.println(" __________________________");
+            System.out.println("|                          |");
+            System.out.println(" --------------------------");}
+            System.out.println("Hola");
+    }
+}
+
